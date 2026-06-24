ada8fed922802d150fd9208f591b058dbbd807d5
Fernando Anzisi <roccoanzisi2009@gmail.com>
2026-06-23 21:51:17 -0300

    v43: modo lectura adaptado a tablet, quitar debug

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Incrementar esta versión en cada deploy para limpiar el caché viejo
-const CACHE = 'lector-v42';
+const CACHE = 'lector-v43';
 const PRECACHE = ['/', '/index.html', '/manifest.json', '/libros-data.js'];
 
 self.addEventListener('install', e => {
