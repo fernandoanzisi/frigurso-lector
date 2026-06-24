9cc4106feda8f58b7c41b6c505aeb41cb652261b
Fernando Anzisi <roccoanzisi2009@gmail.com>
2026-06-24 19:52:43 -0300

    v61: debug texto página 5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Incrementar esta versión en cada deploy para limpiar el caché viejo
-const CACHE = 'lector-v60';
+const CACHE = 'lector-v61';
 const PRECACHE = ['/', '/index.html', '/manifest.json', '/libros-data.js'];
 
 self.addEventListener('install', e => {
