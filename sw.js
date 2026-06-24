de44011c92727c7dee21cca37ac3df88c0904aaf
Fernando Anzisi <roccoanzisi2009@gmail.com>
2026-06-23 21:35:07 -0300

    v38: aplicar tamaños tablet via inline styles JS

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Incrementar esta versión en cada deploy para limpiar el caché viejo
-const CACHE = 'lector-v37';
+const CACHE = 'lector-v38';
 const PRECACHE = ['/', '/index.html', '/manifest.json', '/libros-data.js'];
 
 self.addEventListener('install', e => {
